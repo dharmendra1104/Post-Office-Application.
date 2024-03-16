4464b97bbaa78969b731ef2a48ee44c67000850e
dharmendra1104 <dharmendra11042@gmail.com>
2024-03-16 20:31:14 +0200

    beginning

---
+++ index.js
@@ -0,0 +1,23 @@
+let url = "https://jsonip.com"
+let ipAddress = document.querySelector(".ip")
+let btn = document.querySelector(".btn")
+
+function fetcchdata(url) {
+  fetch(url).then((res) => res.json()).then((data) => {
+    console.log(data.ip)
+    ipAddress.textContent = data.ip
+    fetch(`https://ipapi.co/${data.ip}/json/`).then((res) => res.json()).then((data)=>{
+      console.log(data.postal);
+      fetch(`https://api.postalpincode.in/pincode/${data.postal}`)
+    })
+  })
+    .catch(error => {
+      console.error('Error fetching IP address:', error);
+    });
+}
+fetcchdata(url)
+
+
+
+let timezone=new Date().toLocaleString('En-us',{timeZone: 'Asia/Shanghai'} )
+console.log(timezone);
